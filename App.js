796485b5e1e51441d717fe3adecdb6b0e0a48e62
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-11-14 02:41:10 +0800

    Create new game logic when game over

--- App.js
+++ App.js
@@ -10,13 +10,17 @@ export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
-  const gameOverHandler = (numOfRounds) => {
-    setGuessRounds(numOfRounds);
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
   };
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
-    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
@@ -25,7 +29,13 @@ export default function App() {
       <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
     );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />;
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onNewGame={configureNewGameHandler}
+      />
+    );
   }
 
   return (
